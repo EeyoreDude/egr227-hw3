3b99ec86c3f54884f778e832b41393223f343d05
Tyler Adcock <tylerjoseph.adcock@calbaptist.edu>
2021-03-24 18:44:03 -0700

    3/24/21

--- src/AssassinManager.java
+++ src/AssassinManager.java
@@ -1,12 +1,118 @@
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.List;
+
 public class AssassinManager {
-    // YOUR CODE GOES HERE
+    AssassinNode root;
+    AssassinNode grave;
 
+    public AssassinManager(List<String> names) {
+        if(names.isEmpty())
+            throw new IllegalArgumentException("The list of names is either empty or null");
+        root = createKillRing(names);
+        grave = null;
+    }
 
+    private AssassinNode createKillRing(List<String> names) {
+            if (names.size() > 1)
+                return new AssassinNode(names.get(0), createKillRing(names.subList(1, names.size())));
+            else
+                return new AssassinNode(names.get(0));
+    }
 
+    public void printKillRing(){
+       stalkList(root);
+    }
 
+    private void stalkList(AssassinNode currNode){
+        if(root.next == null)
+            System.out.println("    " + currNode.name + " won the game!");
+        else if(currNode.next == null)
+            System.out.println("    " + currNode.name + " is stalking " + root.name);
+        else {
+            System.out.println("    " + currNode.name + " is stalking " + currNode.next.name);
+            stalkList(currNode.next);
+        }
+    }
 
+    public void printGraveyard(){
+        graveList(grave);
+    }
 
+    private void graveList(AssassinNode currNode){
+        if(currNode != null) {
+            if (currNode.next == null)
+                System.out.println("    " + currNode.name + " was killed by " + currNode.killer);
+            else {
+                System.out.println("    " + currNode.name + " was killed by " + currNode.killer);
+                graveList(currNode.next);
+            }
+        }
 
+    }
+
+    public boolean killRingContains(String name){
+        return contains(root, name);
+    }
+
+    public boolean graveyardContains(String name){
+        return contains(grave, name);
+    }
+
+    private boolean contains(AssassinNode currNode, String name){
+        if(currNode == null)
+            return false;
+        else if(currNode.name.equalsIgnoreCase(name))
+            return true;
+        else
+            return contains(currNode.next, name);
+    }
+
+    public boolean isGameOver(){
+        return root.next == null;
+    }
+
+    public String winner(){
+        if(isGameOver())
+            return root.name;
+        else
+            return null;
+    }
+
+    public void kill(String name){
+        if(isGameOver())
+            throw new IllegalStateException("The game is over.");
+        if(! killRingContains(name))
+            throw new IllegalArgumentException(name + " is not part of the kill ring.");
+
+        killName(root, name);
+    }
+
+    private void killName(AssassinNode currNode, String name){
+        if(currNode.next == null){ // if the killer is the last in the list (the root died)
+            root.killer = currNode.name; // make the current person the killer of the root
+            AssassinNode tempNode = root; // save the location of the root
+            root = root.next; // make the new root the next player
+            if(grave == null){ // on the first kill
+                tempNode.next = null; // make the dead person point to null
+                grave = tempNode; // make the dead person the root of the graveyard
+            } else {
+                tempNode.next = grave;
+                grave = tempNode;
+            }
+        } else if(currNode.next.name.equalsIgnoreCase(name)){ // if the next person in the list is the name (aka we found the killer)
+            currNode.next.killer = currNode.name; // make the current person the killer of the next person
+            AssassinNode tempNode = currNode.next; // save the location of the dead person
+            currNode.next = currNode.next.next; // give the current person the dead persons target
+            if(grave == null){ // on the first kill
+                tempNode.next = null; // make the dead person point to null
+                grave = tempNode; // make the dead person the root of the graveyard
+            } else {
+                tempNode.next = grave; //
+                grave = tempNode;
+            }
+        } else
+            killName(currNode.next, name);
+    }
 
     //////// DO NOT MODIFY AssassinNode.  You will lose points if you do. ////////
     /**
@@ -17,7 +123,7 @@ public class AssassinManager {
         public final String name;  // this person's name
         public String killer;      // name of who killed this person (null if alive)
         public AssassinNode next;  // next node in the list (null if none)
-        
+
         /**
          * Constructs a new node to store the given name and no next node.
          */
--- tst/AssassinManagerTest.java
+++ tst/AssassinManagerTest.java
@@ -11,13 +11,106 @@ import java.util.List;
 //Test constructor
 public class AssassinManagerTest {
 
-    /**
-     * Test case 1 provided as an example
-     * Test graveyardContains should not have the person who wasn't killed
-     */
+
     @Test
-    public void graveyardContainsNegtiveTest(){
-        List<String> list1= new ArrayList<String>();
+    public void constructorNegativeTest(){
+        try{
+            List<String> emptyList = new ArrayList<>();
+            AssassinManager manager = new AssassinManager(emptyList);
+            Assert.fail("AssassinManager should throw IllegalArgumentExeption for empty list");
+        }catch (IllegalArgumentException e) {
+        }
+    }
+    @Test
+    public void constructorPositiveTest(){
+        List<String> list = new ArrayList<>();
+        list.add("Grayson");
+        list.add("Ocean");
+        list.add("Chris");
+        list.add("Dr.Han");
+        AssassinManager manager = new AssassinManager(list);
+        Assert.assertNotNull(manager.root);
+    }
+
+
+    @Test
+    public void printKillRingNegativeTest(){
+        List<String> list = new ArrayList<>();
+        list.add("Grayson");
+        list.add("Ocean");
+        list.add("Chris");
+        list.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list);
+        manager.kill("Ocean");
+        manager.kill("Chris");
+        manager.printKillRing();
+    }
+    @Test
+    public void printKillRingPositiveTest(){
+        List<String> list = new ArrayList<>();
+        list.add("Grayson");
+        list.add("Ocean");
+        list.add("Chris");
+        list.add("Dr.Han");
+        AssassinManager manager = new AssassinManager(list);
+        manager.printKillRing();
+    }
+
+
+    @Test
+    public void printGraveyardNegativeTest(){
+        List<String> list = new ArrayList<>();
+        list.add("Grayson");
+        list.add("Ocean");
+        list.add("Chris");
+        list.add("Dr.Han");
+        AssassinManager manager = new AssassinManager(list);
+        manager.printGraveyard();
+    }
+    @Test
+    public void printGraveyardPositiveTest(){
+        List<String> list = new ArrayList<>();
+        list.add("Grayson");
+        list.add("Ocean");
+        list.add("Chris");
+        list.add("Dr.Han");
+        AssassinManager manager = new AssassinManager(list);
+        manager.kill("Ocean");
+        manager.kill("Chris");
+        manager.printGraveyard();
+    }
+
+
+    @Test
+    public void killRingContainsNegativeTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+        Assert.assertFalse(manager.killRingContains("Tyler"));
+        manager.kill("Ocean");
+        Assert.assertFalse(manager.killRingContains("Ocean"));
+    }
+    @Test
+    public void killRingContainsPositiveTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+        Assert.assertTrue(manager.killRingContains("Grayson"));
+    }
+
+
+    @Test
+    public void graveyardContainsNegativeTest(){
+        List<String> list1= new ArrayList<>();
         list1.add("Grayson");
         list1.add("Ocean");
         list1.add("Chris");
@@ -27,19 +120,100 @@ public class AssassinManagerTest {
         manager.kill("Grayson");
         Assert.assertFalse(manager.graveyardContains("ocean"));
     }
-
-    /**
-     * Test case 2 provided as an example
-     * Test constructor with invalid argument
-     * Should throw IllegalArgumentException
-     */
     @Test
-    public void constructorNegativeTest(){
-        try{
-            List<String> emptyList = new ArrayList<String>();
-            AssassinManager manager = new AssassinManager(emptyList);
-            Assert.fail("AssassinManager should throw IllegalArgumentExeption for empty list");
-        }catch (IllegalArgumentException e) {
+    public void graveyardContainsPositiveTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+        manager.kill("Grayson");
+        Assert.assertTrue(manager.graveyardContains("Grayson"));
+    }
+
+
+    @Test
+    public void isGameOverNegativeTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+        manager.kill("Grayson");
+        Assert.assertFalse(manager.isGameOver());
+    }
+    @Test
+    public void isGameOverPositiveTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+
+        AssassinManager manager = new AssassinManager(list1);
+        Assert.assertTrue(manager.isGameOver());
+    }
+
+    @Test
+    public void winnerNegativeTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+
+        AssassinManager manager = new AssassinManager(list1);
+        Assert.assertNull(manager.winner());
+    }
+    @Test
+    public void winnerPositiveTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+
+        AssassinManager manager = new AssassinManager(list1);
+        Assert.assertEquals("Grayson", manager.winner());
+    }
+
+    @Test
+    public void killNegativeTest(){
+        List<String> list1= new ArrayList<>();
+        list1.add("Grayson");
+
+        List<String> list2= new ArrayList<>();
+        list2.add("Grayson");
+        list2.add("Ocean");
+        list2.add("Chris");
+        list2.add("Dr.Han");
+
+        AssassinManager gameOver = new AssassinManager(list1);
+        AssassinManager manager = new AssassinManager(list2);
+
+        try {
+            gameOver.kill("Ocean");
+            Assert.fail("Should throw an error because the game is over");
+        } catch(IllegalStateException e) {
+
         }
+
+        try {
+            manager.kill("Alexander the Great");
+            Assert.fail("Should throw an error because the name is not in the list");
+        } catch(IllegalArgumentException e) {
+
+        }
+
+    }
+    @Test
+    public void killPositiveTest(){
+        List<String> list1= new ArrayList<String>();
+        list1.add("Grayson");
+        list1.add("Ocean");
+        list1.add("Chris");
+        list1.add("Dr.Han");
+
+        AssassinManager manager = new AssassinManager(list1);
+        manager.kill("Grayson");
+        Assert.assertTrue(manager.graveyardContains("Grayson"));
+        manager.kill("Dr.HAN");
+        Assert.assertFalse(manager.killRingContains("Dr.Han"));
     }
 }
